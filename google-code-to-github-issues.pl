87e8b799023f6ccb84f1ba820a5341e7d90730c6
Dave O'Neill <dmo@dmo.ca>
2010-05-21 22:24:37 -0400

    Ongoing hackery to fetch as much issue data from googlecode as possible

--- google-code-to-github-issues.pl
+++ google-code-to-github-issues.pl
@@ -1,10 +1,9 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Net::Google::Code;
-use Net::GitHub;
-use LWP::UserAgent;
-use Text::CSV_XS;
+use Net::Google::Code '0.19';
+use Net::GitHub '0.21';
+use Data::Dumper;
 
 my($gc_proj, $gh_proj) = @ARGV;
 
@@ -17,26 +16,60 @@ chomp($github{token} = `git config github.token`);
 my $code = Net::Google::Code->new(project => $gc_proj);
 my $github = Net::GitHub->new(%github);
 
-for my $id (get_issues($code)) {
-    my $issue = $code->issue;
-    $issue->load($id);
-    my $gh_issue = $github->issue->open($issue->summary, $issue->description);
-    warn "$id -> " . $gh_issue->{number};
+my @closed_statii = qw(
+	Fixed
+	Verified
+	Invalid
+	Duplicate
+	WontFix
+);
+
+for my $issue ( $code->issue->list() ) {
+
+	my $desc_with_user =
+		"Originally filed by " . $issue->reporter . " on " . $issue->reported
+		. "\n\n"
+		. $issue->description();
+
+	my $gh_issue = $github->issue->open($issue->summary, $desc_with_user);
+	warn $issue->id() . " -> " . $gh_issue->{number};
+
+	if( ! $gh_issue->{number} ) {
+		# WTF.
+		die Dumper $gh_issue;
+	}
+
+	# Labels
+	foreach my $label ( @{ $issue->labels } ) {
+		$github->issue->add_label( $gh_issue->{number}, $label );
+	}
+
+	# Comments
+	$issue->load_comments();
+	foreach my $comment (@{ $issue->comments }) {
+		if( $comment->updates ) {
+			if (exists $comment->updates->{labels} ) {
+				foreach ( @{ $comment->updates->{labels} } ) {
+					if( s/^-// ) {
+						$github->issue->remove_label( $gh_issue->{number}, $_);
+					} else {
+						$github->issue->add_label( $gh_issue->{number}, $_);
+					}
+				}
+			}
+			# TODO other updates
+			warn Dumper $comment->updates if $comment->updates;
+		}
+		next unless $comment->content;
+		my $comment_with_user = "Original comment by " . $comment->author . " on " . $comment->date . "\n\n" . $comment->content();
+#		$github->issue->comment($gh_issue->{number}, $comment_with_user);
+	}
+
+	if( grep { $_ eq $issue->status } @closed_statii ) {
+#		$github->issue->close( $gh_issue->{number} );
+	}
+
+	# TODO attachments?
+
+	sleep (2);
 }
-
-sub get_issues {
-    my $ua = LWP::UserAgent->new;
-    my $content = $ua->get("http://code.google.com/p/$gc_proj/issues/csv")->content;
-    open my $io, '<', \$content;
-    my $parser  = Text::CSV_XS->new({ binary => 1 });
-    $parser->column_names($parser->getline($io));
-
-    my @issues;
-    while (my $row = $parser->getline_hr($io)) {
-        last unless $row->{ID};
-        push @issues, $row->{ID};
-    }
-
-    return @issues;
-}
-
